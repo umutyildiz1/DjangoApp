b085842307f6475ca4a5ef3c4cba5ea7a2baff31
yasintas9 <73482260+yasintas9@users.noreply.github.com>
2020-12-30 20:19:57 +0300

    mesajj

--- tamircim/tamircim/urls.py
+++ tamircim/tamircim/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('register/', views.register,name="register"),
     path('homePage/', views.homePage,name="homePage"),
     path('aboutUs/', views.aboutUs,name="aboutUs"),
+     path('mesajj/', views.mesajj,name="mesajj"),
 ]
