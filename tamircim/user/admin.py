7ea77cabc21e3ae45015fe431e139bf5213b4d68
yasintas9 <73482260+yasintas9@users.noreply.github.com>
2021-01-06 13:48:16 +0300

    models
    models

--- tamircim/user/admin.py
+++ tamircim/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ariza,tamirci
+
+admin.site.register(ariza)
+admin.site.register(tamirci)
--- tamircim/user/models.py
+++ tamircim/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ariza(models.Model):
+    user = models.ForeignKey("auth.user",on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    content=models.TextField()
+    created_date=models.DateTimeField(auto_now_add=True)
+
+class tamirci(models.Model):
+    tamirci=models.ForeignKey("auth.user",on_delete=models.CASCADE)
+    service=models.CharField(max_length=25)
+    explanationofservice=models.TextField()
+    created_date=models.DateTimeField(auto_now_add=True)
+
