919e234dfc480fe4d793206992ff0619f63efd8a
umutyildiz <umut.yildiz05@hotmail.com>
2021-01-06 14:35:01 +0300

    models is created

--- tamircim/user/admin.py
+++ tamircim/user/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ariza,tamirci
+from .models import ariza,tamirci,map,message
 
 admin.site.register(ariza)
 admin.site.register(tamirci)
+admin.site.register(map)
+admin.site.register(message)
--- tamircim/user/models.py
+++ tamircim/user/models.py
@@ -14,3 +14,17 @@ class tamirci(models.Model):
     explanationofservice=models.TextField()
     created_date=models.DateTimeField(auto_now_add=True)
 
+class map(models.Model):
+    tamirci=models.ForeignKey("auth.user",on_delete=models.CASCADE)
+    dukkanAdi=models.CharField(max_length=50)
+    il=models.CharField(max_length=50)
+    ilçe=models.CharField(max_length=50)
+    mahalle=models.CharField(max_length=50)
+    cadde=models.CharField(max_length=50)
+    sokak=models.CharField(max_length=50)
+    no=models.CharField(max_length=50)
+
+class message(models.Model):
+    user=models.ForeignKey("auth.user",on_delete=models.CASCADE)
+    to=models.CharField(max_length=250)
+    frrom=models.CharField(max_length=250)
